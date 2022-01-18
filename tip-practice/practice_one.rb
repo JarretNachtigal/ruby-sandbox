9ed8a6590ee5e581294dec61aefb3846caa14fca
Jarret Nachtigal <jarretn1224@gmail.com>
2022-01-18 10:36:27 -0500

    binary search practice

--- tip-practice/practice_one.rb
+++ tip-practice/practice_one.rb
@@ -38,9 +38,33 @@ def bubble_sort(arr)
 end
 arr = [9,8,7,5,6,4,3,1,2]
 # output: [1,2,3,4,5,6,7,8,9]
-p bubble_sort(arr)
-# binary search ---------------
+# p bubble_sort(arr)
 
+# binary search ---------------
+def binary_search(arr, n)
+  sorted_arr = bubble_sort(arr) # sort array
+  # start at halfway point
+  halfway = sorted_arr.length / 2
+  index = halfway
+  checked = {} # hash to hold indexes that have been checked
+  while sorted_arr[index] != n # while n not found
+    if checked[index] # if its not in array and the program is looping, return nil
+      return nil
+    end
+    checked[index] = true # add index to checker hash
+    if sorted_arr[index] > n # if too high
+      p "high #{index}"
+      index = index/2
+    else # if too low
+      p "low #{index}"
+      index = (index + sorted_arr.length) / 2
+    end
+  end
+  return index # after loop ends, return index
+end
+arr = [9,8,7,5,6,4,3,1,2]
+# find index of given number
+p binary_search(arr, -3)
 # selection sort ---------------
 
 # insertion sort ---------------
