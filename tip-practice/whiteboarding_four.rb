b1ed4283d7d1cade05d6610ca09a6c370d6abd6d
Jarret Nachtigal <jarretn1224@gmail.com>
2022-01-27 21:10:49 -0500

    whiteboarding

--- tip-practice/whiteboarding_four.rb
+++ tip-practice/whiteboarding_four.rb
@@ -10,6 +10,7 @@
 
 # can be improved by shoveling the first one automatically so that if it is a close, indexing i = (0) - 1 doesnt break it
 # and start i at 1 not 0, also can be an if ([{ elsif )]} and ifs inside elsif for each pop
+# OR return false immediately if string[0] is a close bracket 
 def valid_string(string)
   i = 0 # index
   arr = [] # stack thing
@@ -52,10 +53,41 @@ def valid_string(string)
   return false
 end
 
-p valid_string("()[]{}")
-p valid_string("()")
-p valid_string("(]")
-p valid_string("](")
+# p valid_string("()[]{}")
+# p valid_string("()")
+# p valid_string("(]")
+# p valid_string("](")
 
-# ()[]{}
-# (
+# find number of digits
+# loop
+# check if each digit is a divisor of n
+# count
+# return count
+
+# input: 124 1 2 4
+# output: 3
+
+def find_digits(n)
+  # find length/ number of digits
+  digits = n.to_s # use this with an index to check each digit individually
+  num_digits = digits.length
+  counter = 0 # count number of divisors
+  # loop
+  i = 0
+  while i < num_digits
+    # if divides evenly add to counter
+    if digits[i].to_i != 0
+      if n % digits[i].to_i == 0
+        counter += 1
+        p digits[i]
+      end
+    end
+    i += 1
+  end
+  # return
+  counter
+end
+
+p find_digits(124)
+p find_digits(111)
+p find_digits(10)
